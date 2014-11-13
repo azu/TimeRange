9ccc2adb74368c6b205b2de66162fb514608dc02
azu <azuciao@gmail.com>
2014-11-13 12:02:18 +0900

    fix(TimeRange): fix header comment

--- Pod/Classes/TimeRange.h
+++ Pod/Classes/TimeRange.h
@@ -11,12 +11,12 @@
     Ref. https://codeiq.jp/magazine/2013/11/593/
  */
 @interface TimeRange : NSObject
-// CAUTION : You should not access directory these property.
+// CAUTION : You should not access direct these property.
 @property(nonatomic) NSDate *aDate;
 @property(nonatomic) NSDate *bDate;
-
+// Initialize method
 + (instancetype)rangeWithStartDate:(NSDate *) startDate endDate:(NSDate *) endDate;
-
+// if contain target date, return YES
 - (BOOL)contains:(NSDate *) targetData;
 // if overlaps between two timeRange, return YES
 - (BOOL)overlaps:(TimeRange *) anotherTimeRange;
